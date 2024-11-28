d1de655df6bb3cfd4c0be7c4c41e620e6bf9be97
Yaitza1 <ridhoananda100@gmail.com>
2024-11-28 19:21:09 +0700

    menambahkan fitur kecil pada Main menu

--- Assets/Scenes/SceneManager/MainMenu.cs
+++ Assets/Scenes/SceneManager/MainMenu.cs
@@ -5,11 +5,35 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    private float lastMouseMoveTime;
+    private float cursorHideDelay = 2.0f;
+    
     void Start()
     {
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.None;
+        lastMouseMoveTime = Time.time;
     }
+
+    void Update()
+    {
+        // Deteksi gerakan mouse
+        if (Input.GetAxis("Mouse X") != 0 || Input.GetAxis("Mouse Y") != 0)
+        {
+            lastMouseMoveTime = Time.time;
+            if (!Cursor.visible)
+            {
+                Cursor.visible = true; // Perlihatkan kursor jika sebelumnya tersembunyi
+            }
+        }
+
+        // Sembunyikan kursor jika tidak ada gerakan dalam waktu tertentu
+        if (Time.time - lastMouseMoveTime > cursorHideDelay)
+        {
+            Cursor.visible = false;
+        }
+    }
+
     public void PlayGame()
     {
         SceneManager.LoadSceneAsync("Level 1");
